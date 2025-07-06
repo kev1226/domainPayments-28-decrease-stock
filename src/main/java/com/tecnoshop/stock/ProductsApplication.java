1dfaf30f9a1c974dc26d8897c552e3af58fd7cec
kev1226 <kevin_alt12@hotmail.com>
2025-07-06 17:21:15 -0500

    implement CORS configuration and Swagger documentation setup

--- src/main/java/com/tecnoshop/stock/ProductsApplication.java
+++ src/main/java/com/tecnoshop/stock/ProductsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DecreaseStockApplication {
+public class ProductsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DecreaseStockApplication.class, args);
+		SpringApplication.run(ProductsApplication.class, args);
 	}
 }
--- src/test/java/com/tecnoshop/stock/ProductsApplicationTests.java
+++ src/test/java/com/tecnoshop/stock/ProductsApplicationTests.java
@@ -1,9 +1,9 @@
-package com.tecnoshop.products;
+package com.tecnoshop.stock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProductsApplication.class)
 class ProductsApplicationTests {
 
 	@Test
